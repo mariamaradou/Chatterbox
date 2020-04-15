799c09dd3ebadcf21947bca17480dda7d5c00270
Maria Maradou <61742470+mariamaradou@users.noreply.github.com>
2020-04-15 21:33:17 +0300

    Update javamessenger.js

--- javamessenger.js
+++ javamessenger.js
@@ -3,9 +3,49 @@
 
 
 function moveMessage () {
-    console.log("Το κουμπί πατήθηκε");
-    
-    
+  console.log("Το κουμπί πατήθηκε");
+  
+  
+  var Value=document.getElementById("typewrite").value;
+  document.getElementById("typewrite").value = "";
+  
+  if (Value==='') {
+  alert("You didn't type anything!");} 
+  else {   
+      var p=document.createElement("p");
+ var text=document.createTextNode(Value); //apothikeuw to text
+ p.appendChild(text);
+document.getElementById("mylist").appendChild(p); }
+
+window.setInterval(function() {
+  var elem = document.getElementById("mylist");
+  elem.scrollTop = elem.scrollHeight;
+}, 4000);
+
+
+ } 
+  
+ var getImage = function(event) {
+
+  
+  var image = document.getElementById('photoimage');
+image.src = URL.createObjectURL(event.target.files[0]);
+
+ }
+
+ var getImage1 = function(event) {
+
+  
+  var imageOne = document.getElementById('photoimage1');
+imageOne.src = URL.createObjectURL(event.target.files[0]);
+
+ }
+
+ function sendMessage() {
+  var key = window.event.keyCode;
+
+  // If the user has pressed enter
+  if (key === 13) {
     var Value=document.getElementById("typewrite").value;
     document.getElementById("typewrite").value = "";
     
@@ -21,10 +61,11 @@ function moveMessage () {
     var elem = document.getElementById("mylist");
     elem.scrollTop = elem.scrollHeight;
   }, 4000);
+      return false;
+  }
+  else {
+      return true;
+  }
+}
 
   
-   
-   } 
-    
-
-    
