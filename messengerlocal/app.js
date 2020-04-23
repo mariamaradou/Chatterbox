5bc16bd29c505fe0f62b45a34d1667ac17269170
Maria Maradou <61742470+mariamaradou@users.noreply.github.com>
2020-04-23 03:12:43 +0300

    Update app.js

--- messengerlocal/app.js
+++ messengerlocal/app.js
@@ -1,6 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var multer = require('multer');
+const path = require("path");
+const hbs=require('hbs');
+const bodyParser = require('body-parser');
+
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
@@ -14,6 +19,7 @@ io.on('connection', function(socket) {
       console.log(data);
       
       if(users.indexOf(data) > -1) {
+         //send data with socket.emit
          socket.emit('userExists', data + ' username is taken! Try some other username.');
       } else {
          users.push(data);
@@ -27,7 +33,108 @@ io.on('connection', function(socket) {
       //Send message to everyone
       io.sockets.emit('newmsg', data);
    })
+
+  /* socket.on('profilepic',function(data) {
+      //send profilepic to everyone
+      
+      io.sockets.emit('profpic',data);
+   }) 
+ */
+
+ socket.on('status',function(data){
+    io.sockets.emit('statusnew',data);
+    
+ })
+
+/* app.set('view engine', hbs);
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res) => {
+   res.render('index.hbs');
 });
+
+app.post('/send',(req,res) => {
+   console.log(req.body);
+}); */
+
+
+
+});
+
+
+// GIA UPLOAD FILES
+// View Engine Setup 
+/* app.set("views",path.join(__dirname,"views")) 
+app.set("view engine","ejs") 
+    
+// var upload = multer({ dest: "Upload_folder_name" }) 
+// If you do not want to use diskStorage then uncomment it 
+    
+var storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+  
+        // Uploads is the Upload_folder_name 
+        cb(null, "uploads") 
+    }, 
+    filename: function (req, file, cb) { 
+      cb(null, file.fieldname + "-" + Date.now()+".jpg") 
+    } 
+  }) 
+       
+// Define the maximum size for uploading 
+// picture i.e. 1 MB. it is optional 
+const maxSize = 1 * 1000 * 1000; 
+    
+var upload = multer({  
+    storage: storage, 
+    limits: { fileSize: maxSize }, 
+    fileFilter: function (req, file, cb){ 
+    
+        // Set the filetypes, it is optional 
+        var filetypes = /jpeg|jpg|png/; 
+        var mimetype = filetypes.test(file.mimetype); 
+  
+        var extname = filetypes.test(path.extname( 
+                    file.originalname).toLowerCase()); 
+        
+        if (mimetype && extname) { 
+            return cb(null, true); 
+        } 
+      
+        cb("Error: File upload only supports the "
+                + "following filetypes - " + filetypes); 
+      }  
+  
+// mypic is the name of file attribute 
+}).single("mypic");        
+  
+app.get("/",function(req,res){ 
+    res.render("Signup"); 
+}) 
+    
+app.post("/uploadProfilePicture",function (req, res, next) { 
+        
+    // Error MiddleWare for multer file upload, so if any 
+    // error occurs, the image would not be uploaded! 
+    upload(req,res,function(err) { 
+  
+        if(err) { 
+  
+            // ERROR occured (here it can be occured due 
+            // to uploading image of size greater than 
+            // 1MB or uploading different file type) 
+            res.send(err) 
+        } 
+        else { 
+  
+            // SUCCESS, image successfully uploaded 
+            res.send("Success, Image uploaded!") 
+        } 
+    }) 
+}) */
+
+
+
 
 http.listen(3000, function() {
    console.log('listening on localhost:3000');
